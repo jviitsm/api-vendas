b34e66e6fc56435cdbedd3cc2ee312e29c4885bd
jviitsm <jvictorgmodesto@gmail.com>
2022-06-10 12:21:10 -0300

    ajuste eslint

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response, NextFunction } from 'express';
-import cors from 'cors'
+import cors from 'cors';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
 
@@ -10,20 +10,21 @@ app.use(express.json());
 
 app.use(routes);
 
-app.use((error: Error, request: Request, respone: Response, next: NextFunction) => {
-  if (error instanceof AppError) {
-    return respone.status(error.statusCode).json({
+app.use(
+  (error: Error, request: Request, respone: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return respone.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+    return respone.status(500).json({
       status: 'error',
-      message: error.message,
+      message: 'Internal Server Error',
     });
-  }
-  return respone.status(500).json({
-    status: 'error',
-    message: 'Internal Server Error',
-  });
-});
-
+  },
+);
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
-})
+});
