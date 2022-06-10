6012dc5b31635592c8e0aa3128bc7e54a12e028f
jviitsm <jvictorgmodesto@gmail.com>
2022-06-10 14:47:17 -0300

    ListProductService implementado

---
+++ src/modules/products/services/ListProductService.ts
@@ -0,0 +1,15 @@
+import { ProductsRepository } from '../typeorm/entities/repositories/ProductsRepository';
+import { getCustomRepository } from 'typeorm';
+import Product from '../typeorm/entities/Product';
+
+class ListProductService {
+  public async execute(): Promise<Product[]> {
+    const productsRepository = getCustomRepository(ProductsRepository);
+
+    const products = await productsRepository.find();
+
+    return products;
+  }
+}
+
+export default ListProductService;
