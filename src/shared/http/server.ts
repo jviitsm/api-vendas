0565b05f5646fc4b5fcb4ae9b344d9e5695a92db
jviitsm <jvictorgmodesto@gmail.com>
2022-06-10 22:23:45 -0300

    celebrate implementado na rota de products

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -2,15 +2,18 @@ import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
+import { errors } from 'celebrate';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
 import '@shared/typeorm';
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+app.use(errors());
 
 app.use(
   (error: Error, request: Request, respone: Response, next: NextFunction) => {
