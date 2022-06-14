5d94c180b61f05f88d240789a42b4f3f300d4efc
jviitsm <jvictorgmodesto@gmail.com>
2022-06-13 22:41:51 -0300

    rota de upload de avatar implementada

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -2,9 +2,15 @@ import { Router } from 'express';
 import { celebrate, Segments, Joi } from 'celebrate';
 import UsersController from '../controllers/UsersController';
 import isAuthenticated from '../../../shared/http/middlewares/isAuthenticated';
+import multer from 'multer';
+import uploadConfig from '@config/upload';
+import UserAvatarController from '../controllers/UserAvatarController';
 
 const usersRouter = Router();
 const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
+
+const upload = multer(uploadConfig);
 
 usersRouter.get('/', isAuthenticated, usersController.index);
 
@@ -18,6 +24,13 @@ usersRouter.post(
     },
   }),
   usersController.create,
+);
+
+usersRouter.patch(
+  '/avatar',
+  isAuthenticated,
+  upload.single('avatar'),
+  userAvatarController.update,
 );
 
 export default usersRouter;
