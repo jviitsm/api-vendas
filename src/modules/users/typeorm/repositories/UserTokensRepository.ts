bd4d45591be4b79fe310265f39ce970052d2ed36
jviitsm <jvictorgmodesto@gmail.com>
2022-06-14 15:12:37 -0300

    entidade e repositorio de userToken

---
+++ src/modules/users/typeorm/repositories/UserTokensRepository.ts
@@ -0,0 +1,20 @@
+import { EntityRepository, Repository } from 'typeorm';
+import UserToken from '../entities/UserToken';
+
+@EntityRepository(UserToken)
+export default class UserTokensRepository extends Repository<UserToken> {
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const user = await this.findOne({ where: { token } });
+    return user;
+  }
+
+  public async findById(userId: string): Promise<UserToken | undefined> {
+    const userToken = await this.create({
+      user_id: userId,
+    });
+
+    await this.save(userToken);
+
+    return userToken;
+  }
+}
