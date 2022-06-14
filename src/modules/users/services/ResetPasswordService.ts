cee99138ae6b0587b87a59d2af3bcb6f70cd7516
jviitsm <jvictorgmodesto@gmail.com>
2022-06-14 15:35:03 -0300

    initial ResetPasswordService

---
+++ src/modules/users/services/ResetPasswordService.ts
@@ -0,0 +1,42 @@
+import { getCustomRepository } from 'typeorm';
+import AppError from '@shared/errors/AppError';
+import { isAfter, addHours } from 'date-fns';
+import UserTokensRepository from '../typeorm/repositories/UserTokensRepository';
+import UserRepository from '../typeorm/repositories/UsersRepository';
+import { hash } from 'bcryptjs';
+
+interface IRequest {
+  token: string;
+  password: string;
+}
+
+class ResetPasswordService {
+  public async execute({ token, password }: IRequest): Promise<void> {
+    const usersRepository = getCustomRepository(UserRepository);
+    const userTokensRepository = getCustomRepository(UserTokensRepository);
+
+    const userToken = await userTokensRepository.findByToken(token);
+
+    if (!userToken) {
+      throw new AppError('User token does not exists.');
+    }
+
+    const user = await usersRepository.findById(userToken.user_id);
+
+    if (!user) {
+      throw new AppError('User does not exists.');
+    }
+
+    const compareDate = addHours(userToken.created_at, 2);
+
+    if (isAfter(Date.now(), compareDate)) {
+      throw new AppError('Token expired.');
+    }
+
+    user.password = await hash(password, 8);
+
+    return;
+  }
+}
+
+export default ResetPasswordService;
